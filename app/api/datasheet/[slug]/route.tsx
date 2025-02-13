dd0a2a98ae750f0fccc95571389878755a235c62
cousepig <cousepig@gmail.com>
2025-02-13 12:08:52 +0800

    fix pdf 3

--- app/api/datasheet/[slug]/route.tsx
+++ app/api/datasheet/[slug]/route.tsx
@@ -8,12 +8,12 @@ export async function GET(
   { params }: { params: Promise<{ slug: string }> }
 ) {
   let slug = (await params).slug.replace(".pdf", ".yml");
-  // const chromiumPack = "https://github.com/Sparticuz/chromium/releases/download/v121.0.0/chromium-v121.0.0-pack.tar";
+  const chromiumPack = "chromium-v121.0.0-pack.tar";
   // const browser = await launch();
   const browser = await puppeteer.launch({
     args: chromium.args,
     // See https://www.npmjs.com/package/@sparticuz/chromium#running-locally--headlessheadful-mode for local executable path
-    // executablePath: await chromium.executablePath(chromiumPack),
+    executablePath: await chromium.executablePath(chromiumPack),
     headless: true,
   });
   const page = await browser.newPage();
